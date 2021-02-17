aee150dc3d3095c21f4b3eb77b767718c2ede064
Sofia Lopez <s.lopezg@uniandes.com>
2021-02-17 18:54:48 -0500

    cambios del Estudiante 1

--- App/view.py
+++ App/view.py
@@ -42,6 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
+    print("3-Cargar Book-Tags!")
     # TO-DO: Modificación de Est-1 en el Lab 2
     print("0- Salir")
 
@@ -63,7 +64,9 @@ def loadBookTags():
     """
     Cargar los Tags de libros
     """
-    # TO-DO: Modificación de Est-1 en el Lab 2
+    # TO-DO: Modificación de Est-1 en el Lab 2   
+    return controller.loadBooks('GoodReads/book_tags-small.csv')
+    
 
 
 """
@@ -78,7 +81,7 @@ while True:
         print('Total de libros cargados: ' + str(lt.size(books)))
 
         # TO-DO: Modificación de Est-1 en el Lab 2
-
+        print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
         # TO-DO: Modificación de Est-2 en el Lab 2
 
